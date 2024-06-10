79fa161eef28bb1f0d527d909e3849d3029ec8b2
mikalai-b <mikalai.bahdanau@gmail.com>
2024-06-10 14:03:09 +0200

    [promo] - updated OrderController

--- src/Order/Adapter/Symfony/Controller/OrderController.php
+++ src/Order/Adapter/Symfony/Controller/OrderController.php
@@ -3,8 +3,10 @@
 namespace App\Order\Adapter\Symfony\Controller;
 
 
+use App\Order\Application\UseCase\AddPromotion;
 use App\Order\Application\UseCase\CreateOrder;
 use App\Order\Application\UseCase\AddOrderItem;
+use App\Order\Application\UseCase\GetOrder;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -31,5 +33,22 @@ class OrderController extends AbstractController
     {
         $addOrderItem->execute($orderId, $productId);
         return new JsonResponse();
+    }
+
+    #[Route('/add_promo/{orderId}/{promoId}', name: 'add_promo', methods: ['PUT'])]
+    public function addPromotionAction(int $orderId, int $promoId, AddPromotion $addPromotion): JsonResponse
+    {
+        $addPromotion->execute($orderId, $promoId);
+        return new JsonResponse();
+    }
+
+    /**
+     * @throws \Exception
+     */
+    #[Route('/order/{orderId}', name: 'get_order', methods: ['GET'])]
+    public function getOrderAction(int $orderId, GetOrder $getOrder): JsonResponse
+    {
+        $orderResponse = $getOrder->execute($orderId);
+        return new JsonResponse($orderResponse);
     }
 }
